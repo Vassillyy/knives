022611774b57f96b99c407e84e7b940db8881355
Василий <roman12345fak@gmail.com>
2026-07-25 15:07:36 +0300

    refactor embed

--- internal/repository/queries/embed.go
+++ internal/repository/queries/embed.go
@@ -2,7 +2,6 @@ package queries
 
 import (
 	"embed"
-	"fmt"
 	"regexp"
 	"strings"
 )
@@ -17,56 +16,23 @@ type Queries struct {
 }
 
 func (q *Queries) Get(name string) string {
-	query, ok := q.queries[name]
-	if !ok {
-		panic(fmt.Sprintf("query %q not found", name))
-	}
-	return query
+	return q.queries[name]
 }
 
 func MustLoad(filename string) *Queries {
-	q, err := load(filename)
-	if err != nil {
-		panic(err)
-	}
-	return q
-}
-
-func load(filename string) (*Queries, error) {
-	content, err := sqlFiles.ReadFile(filename)
-	if err != nil {
-		return nil, fmt.Errorf("read sql file %s: %w", filename, err)
-	}
-
-	queries, err := parse(string(content))
-	if err != nil {
-		return nil, fmt.Errorf("parse sql file %s: %w", filename, err)
-	}
-
-	return &Queries{queries: queries}, nil
-}
-
-func parse(content string) (map[string]string, error) {
+	content, _ := sqlFiles.ReadFile(filename)
 	queries := make(map[string]string)
-	matches := queryNameRegex.FindAllStringSubmatchIndex(content, -1)
-	if len(matches) == 0 {
-		return nil, fmt.Errorf("no queries found")
-	}
+	matches := queryNameRegex.FindAllStringSubmatchIndex(string(content), -1)
 
 	for i, match := range matches {
-		nameStart, nameEnd := match[2], match[3]
-		name := content[nameStart:nameEnd]
-
+		name := string(content[match[2]:match[3]])
 		queryStart := match[1]
-		var queryEnd int
+		queryEnd := len(content)
 		if i+1 < len(matches) {
 			queryEnd = matches[i+1][0]
-		} else {
-			queryEnd = len(content)
 		}
-
-		query := strings.TrimSpace(content[queryStart:queryEnd])
-		queries[name] = query
+		queries[name] = strings.TrimSpace(string(content[queryStart:queryEnd]))
 	}
-	return queries, nil
+
+	return &Queries{queries: queries}
 }
